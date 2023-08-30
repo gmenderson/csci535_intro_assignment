1ea4f6171b5704a32a4b28fcb6799d543a545967
Grant Enderson <gmenderson@wm.edu>
2023-08-30 19:17:09 -0400

    draft xml parse script

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,15 @@
 # Program to parse the xml files to find leaf GUI components
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('/data/')
+def find_xml_leaves(node):
+    if len(node) == 0: 
+        print("Leaf node:", node.tag)
+    else:
+        for child in node: 
+            find_xml_leaves(child)
+
+tree = ET.parse('/home/grenders95/software_engineering/csci535_intro_assignment/data/com.dropbox.android.xml')
+root = tree.getroot()
+
+find_xml_leaves(root)
+
