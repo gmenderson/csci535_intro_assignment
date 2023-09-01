48f5dec5de2a3523a9691ff46e34869e8b147c5e
Grant Enderson <gmenderson@wm.edu>
2023-09-01 12:46:16 -0400

    edited src for testing

--- src/parser.py
+++ src/parser.py
@@ -1,15 +1,32 @@
-# Program to parse the xml files to find leaf GUI components
 import xml.etree.ElementTree as ET
+from PIL import Image, ImageDraw
 
-def find_xml_leaves(node):
-    if len(node) == 0: 
-        print("Leaf node:", node.tag)
-    else:
-        for child in node: 
-            find_xml_leaves(child)
-
-tree = ET.parse('/home/grenders95/software_engineering/csci535_intro_assignment/data/com.dropbox.android.xml')
+# Load the XML hierarchy from a file
+xml_file_path = "/home/grenders95/software_engineering/csci535_intro_assignment/data/com.yelp.android.xml"
+tree = ET.parse(xml_file_path)
 root = tree.getroot()
 
-find_xml_leaves(root)
+# Capture a screenshot of the app's UI (replace with your screenshot capturing logic)
+screenshot = Image.open("/home/grenders95/software_engineering/csci535_intro_assignment/data/com.yelp.android.png")
+
+# Create an ImageDraw object to annotate the screenshot
+draw = ImageDraw.Draw(screenshot)
+
+# Function to check if a node is a leaf node
+def is_leaf(node):
+    return len(list(node)) == 0
+
+# Loop through the nodes in the XML hierarchy and highlight leaf components
+for node in root.iter("node"):
+    if is_leaf(node):
+        bounds = node.get("bounds")
+        try:
+            left, top, right, bottom = map(int, bounds.strip("[]").split(","))
+            draw.rectangle([left, top, right, bottom], outline="red", width=2)
+        except ValueError:
+            print(f"Skipping node with invalid bounds: {bounds}")
+
+# Save or display the annotated screenshot
+screenshot.show()
+#screenshot.save("annotated_screenshot.png")
 
