78e17edd1f4d409798ed30cf18783de361cf8177
Grant Enderson <gmenderson@wm.edu>
2023-09-03 15:44:15 -0400

    added more to script

--- src/parser.py
+++ src/parser.py
@@ -1,35 +1,28 @@
 from PIL import Image, ImageDraw
 import xml.etree.ElementTree as ET
 
-# Load the XML file
-tree = ET.parse('/home/grenders95/software_engineering/csci535_intro_assignment/data/com.giphy.messenger-1.xml')
-root = tree.getroot()
+# Function to parse the XML file
+def parse_xml(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    return root
 
 # Function to recursively find all leaf nodes
 def find_leaf_nodes(node, leaf_nodes):
-    children = list(node)
-    if not children:
-        leaf_nodes.append(node)
+    if len(node) == 0: 
+        if 'bounds' in node.attrib:
+            leaf_nodes.append(node.attrib['bounds'])
     else:
-        for child in children:
-            find_leaf_nodes(child, leaf_nodes)
+        for child in node: 
+            find_leaf_nodes(child, leaf_nodes) 
 
-# Initialize a list to store leaf nodes
-leaf_nodes = []
+if __name__ == "__main__":
+    print("Enter the path of the XML file to parse:")
+    xml_file = input()
+    print("Enter the path of the PNG file to be annotated:")
+    png_file = input()
+    
+    root = parse_xml(xml_file)
 
-# Start the search for leaf nodes from the root node
-find_leaf_nodes(root, leaf_nodes)
-
-# Load the existing screenshot image
-screenshot = Image.open('/home/grenders95/software_engineering/csci535_intro_assignment/data/com.giphy.messenger-1.png')
-draw = ImageDraw.Draw(screenshot)
-
-# Iterate through the leaf nodes, extract bounds, and draw bounding boxes
-for node in leaf_nodes:
-    bounds = node.get('bounds').strip('[]').split('][')
-    left, top = map(int, bounds[0].split(','))
-    right, bottom = map(int, bounds[1].split(','))
-    draw.rectangle([left, top, right, bottom], outline="yellow", width=3)
-
-# Save the modified image with bounding boxes
-screenshot.save('screenshot_with_boxes.png', 'PNG')
+    leaf_nodes = []
+    find_leaf_nodes(root, leaf_nodes)
